4074c7f6355b313cb3e892c18b2f4f9d44f846aa
AadidevSoni <aadidevbahrain@gmail.com>
2025-01-30 17:17:04 +0530

    Hovering Player (No sprites)

--- src/Entity.java
+++ src/Entity.java
@@ -1,6 +1,12 @@
+import java.awt.image.BufferedImage;
+
 public class Entity {
 
   public int x,y;
   public int speed;
+
+  //we use bufferImage to store image data
+	public BufferedImage up1,up2,down1,down2,left1,left2,right1,right2;
+	public String direction;
   
 }
